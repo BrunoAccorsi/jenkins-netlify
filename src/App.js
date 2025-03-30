cfbce11c741b5f18c4fe97324756550c4c6758da
Bruno Accorsi Bergoli <b.bergoli@gmail.com>
2025-03-30 14:32:53 -0600

    update header in App component with course details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 function App() {
-  return <h1>Bruno Accorsi Bergoli</h1>;
+  return (
+    <h1>Bruno Accorsi Bergoli - TECH2102: Enterprise Computing - 25JANMNFS2</h1>
+  );
 }
 
 export default App;
